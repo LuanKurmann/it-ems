9d85c8005f05ef74e720fd4d8e4758bf78e14189
Lehmee <luca.lehmann@cubetech.ch>
2022-11-29 14:00:33 +0100

    inital commit

--- app/controllers/ArticleController.php
+++ app/controllers/ArticleController.php
@@ -8,12 +8,10 @@ use PDO;
 
 class ArticleController
 {
-    // Show the article attributes based on the id.
 	public function showAction(int $id, RouteCollection $routes)
 	{
         $article = new Article();
         $pdo = new PDO('mysql:host=' . constant('DB_HOST') . ';' . 'dbname=' . constant('DB_NAME'), constant('DB_USER'), constant('DB_PASS'));
-
         $sql = "SELECT * FROM article WHERE id=$id";
         foreach ($pdo->query($sql) as $row) {
             $article->setTitle($row['title']);
@@ -21,18 +19,9 @@ class ArticleController
             $article->setAmount($row['amount']);
             $article->setColor($row['color']);
             $article->setBrand($row['brand']);
+            $article->setImage($row['image']);
         }
         $article->read($id);
-
-
-
-        
-
-
-        
-
         require_once APP_ROOT . '/views/article.php';
 	}
-
-
 }
